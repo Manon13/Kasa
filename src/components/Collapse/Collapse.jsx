3df5807f57db679df7e6dee97b5767c12795a57d
Manon <manonn.goliot@gmail.com>
2024-06-17 18:27:56 +0200

    collapse creation

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,17 @@
+import data from "../../../a_propos.json";
+import "./Collapse.sass";
+
+function Collapse() {
+    return  (
+        <div className="collapse">
+            {data.map((item, index) => (
+                <div key={index} className="collapse__item">
+                    <h2 className="collapse__title">{item.title}</h2>
+                    <p className="collapse__description">{item.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Collapse;
--- src/pages/A_propos.jsx
+++ src/pages/A_propos.jsx
@@ -1,9 +1,11 @@
 import Banner_a_propos from '../components/Banner_a_propos/Banner_a_propos';
+import Collapse from '../components/Collapse/Collapse';
 
 function A_propos() {
     return (
         <>
         <Banner_a_propos />
+        <Collapse />
         </>
     );
 
