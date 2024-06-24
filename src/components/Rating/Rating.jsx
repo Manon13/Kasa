29b1dfe9462cf3e6d0104b580a64f63cb0cd6042
Manon <manonn.goliot@gmail.com>
2024-06-24 17:41:48 +0200

    responsive mobile logement page

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -3,27 +3,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './Rating.sass'; 
 
-const Rating = ({ rating }) => {
-  const renderStars = () => {
-    const stars = [];
-    for (let i = 1; i <= 5; i++) {
-      stars.push(
-        <FontAwesomeIcon
-          key={i}
-          icon={faStar}
-          className={i <= rating ? 'star-icon full-star' : 'star-icon empty-star'}
-        />
-      );
-    }
-    return stars;
-  };
+function renderStars(rating) {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <FontAwesomeIcon
+        key={i}
+        icon={faStar}
+        className={i <= rating ? 'star-icon full-star' : 'star-icon empty-star'}
+      />
+    );
+  }
+  return stars;
+}
 
-  return <div className="rating">{renderStars()}</div>;
-};
+function Rating(props) {
+  const { rating } = props;
+  return <div className="rating">{renderStars(rating)}</div>;
+}
 
 Rating.propTypes = {
   rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
-
