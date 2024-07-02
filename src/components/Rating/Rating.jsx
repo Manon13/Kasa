7baf22331385576234c920be96cfe64fc258954f
Manon <manonn.goliot@gmail.com>
2024-07-02 15:36:28 +0200

    rating changement

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,26 +1,19 @@
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import './Rating.sass'; 
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import './Rating.sass'
 
-function renderStars(rating) {
-  const stars = [];
-  for (let i = 1; i <= 5; i++) {
-    stars.push(
-      <FontAwesomeIcon
-        key={i}
-        icon={faStar}
-        className={i <= rating ? 'star-icon full-star' : 'star-icon empty-star'}
-      />
-    );
-  }
-  return stars;
-}
+const Rating = ({ rating }) => {
+  const stars = Array.from({ length: 5 }, (_, i) => (
+    <FontAwesomeIcon
+      key={i}
+      icon={faStar}
+      className={i < rating ? 'star-icon full-star' : 'star-icon empty-star'}
+    />
+  ));
 
-function Rating(props) {
-  const { rating } = props;
-  return <div className="rating">{renderStars(rating)}</div>;
-}
+  return <div className="rating">{stars}</div>;
+};
 
 Rating.propTypes = {
   rating: PropTypes.number.isRequired,
