9c6c62029d0c200d7b8266e3fbf9b36594c14965
Manon <manonn.goliot@gmail.com>
2024-07-08 12:55:37 +0200

    file renaming

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import { Outlet } from "react-router-dom"
-import './Index.sass'
+import './index.sass'
 
 /**
  * Composant App.
